b6e14c6bfeb0ee28d85c3d52e1b17d21a32f66d4
davydka <davydka@cranberry.local>
2019-02-08 15:37:57 -0500

    good stopping point

--- multi-obj/ViewController.swift
+++ multi-obj/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 import SceneKit
 import ARKit
 
+enum BodyType : Int {
+    case box = 1
+    case plane = 2
+    case car = 3
+}
+
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var planes = [OverlayPlane]()
+    
+    private let label :UILabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,11 +32,123 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
+        // plane stuff
+        sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.label.frame = CGRect(x: 0, y: 0, width: self.sceneView.frame.size.width, height: 44)
+        self.label.center = self.sceneView.center
+        self.label.textAlignment = .center
+        self.label.textColor = UIColor.white
+        self.label.font = UIFont.preferredFont(forTextStyle: .headline)
+        self.label.alpha = 0
+        self.sceneView.addSubview(self.label)
+        // END plane stuff
+        
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        
+        let box = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0)
+        
+        let material = SCNMaterial()
+        material.name = "Color"
+        material.diffuse.contents = UIImage(named: "wood.jpeg")
+        //  material.diffuse.contents = UIColor.blue
+        
+        let boxNode = SCNNode(geometry: box)
+        boxNode.position = SCNVector3(0,0,-0.5)
+        boxNode.geometry?.materials = [material]
+        
+        
+        let sphere = SCNSphere(radius: 0.3)
+        
+        let material2 = SCNMaterial()
+        material2.name = "Color"
+        material2.diffuse.contents = UIImage(named: "earth.jpg")
+        // material2.diffuse.contents = UIColor.red
+        
+        let sphereNode = SCNNode(geometry: sphere)
+        sphereNode.position = SCNVector3(0.5, 0, -0.5)
+        sphereNode.geometry?.materials = [material2]
+        
+        self.sceneView.scene.rootNode.addChildNode(boxNode)
+        self.sceneView.scene.rootNode.addChildNode(sphereNode)
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        tapGestureRecognizer.numberOfTapsRequired = 1
+        let doubleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(doubleTapped))
+        doubleTapGestureRecognizer.numberOfTapsRequired = 2
+        
+        tapGestureRecognizer.require(toFail: doubleTapGestureRecognizer)
+        
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+        self.sceneView.addGestureRecognizer(doubleTapGestureRecognizer)
+    }
+    
+    @objc func doubleTapped(recognizer :UIGestureRecognizer){
+        let sceneView = recognizer.view as! ARSCNView
+        let touchLocation = recognizer.location(in: sceneView)
+        let hitResults = sceneView.hitTest(touchLocation, options: [:])
+        
+        if !hitResults.isEmpty {
+            guard let hitResult = hitResults.first else {
+                return
+            }
+            let node = hitResult.node
+            node.physicsBody?.applyForce(
+                SCNVector3(
+                    hitResult.worldCoordinates.x,
+                    hitResult.worldCoordinates.y,
+                    hitResult.worldCoordinates.z
+                ),
+                asImpulse: true)
+        }
+    }
+    
+    @objc func tapped(recognizer :UIGestureRecognizer) {
+        let sceneView = recognizer.view as! ARSCNView
+        let touchLocation = recognizer.location(in: sceneView)
+        let hitResults = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+        
+        if !hitResults.isEmpty {
+            guard let hitResult = hitResults.first else {
+                return
+            }
+            addBox(hitResult :hitResult)
+        }
+    }
+    
+    /*
+    @objc func tapped(recognizer :UIGestureRecognizer) {
+    let sceneView = recognizer.view as! SCNView
+    let touchLocation = recognizer.location(in: sceneView)
+    let hitResults = sceneView.hitTest(touchLocation, options: [:])
+
+    if !hitResults.isEmpty {
+    let node = hitResults[0].node
+    let material = node.geometry?.material(named: "Color")
+
+    material?.diffuse.contents = UIColor.random()
+    }
+    }
+    */
+    
+    private func addBox(hitResult :ARHitTestResult) {
+        let boxGeometry = SCNBox(width: 0.2, height: 0.2, length: 0.2, chamferRadius: 0)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.random()
+        boxGeometry.materials = [material]
+        let boxNode = SCNNode(geometry: boxGeometry)
+        boxNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        boxNode.physicsBody?.categoryBitMask = BodyType.box.rawValue
+        boxNode.position = SCNVector3(
+            hitResult.worldTransform.columns.3.x,
+            hitResult.worldTransform.columns.3.y + Float(0.5),
+            hitResult.worldTransform.columns.3.z
+        )
+        
+        self.sceneView.scene.rootNode.addChildNode(boxNode)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,9 +156,42 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        configuration.planeDetection = [.horizontal, .vertical]
+        
         // Run the view's session
         sceneView.session.run(configuration)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        DispatchQueue.main.async {
+            self.label.text = "Place Detected"
+            
+            UIView.animate(withDuration: 3.0, animations: {
+                self.label.alpha = 1.0
+            }) { (completion :Bool) in
+                self.label.alpha = 0.0
+            }
+        }
+        
+        if !(anchor is ARPlaneAnchor) {
+            return
+        }
+        
+        let plane = OverlayPlane(anchor: anchor as! ARPlaneAnchor)
+        self.planes.append(plane)
+        node.addChildNode(plane)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        let plane = self.planes.filter { plane in
+            return plane.anchor.identifier == anchor.identifier
+        }.first
+        
+        if plane == nil {
+            return
+        }
+        
+        plane?.update(anchor: anchor as! ARPlaneAnchor)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -47,29 +201,4 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
